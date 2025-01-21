b3f3f6b3bf007a00a9f94d0acce84c791b800370
Salomao Dias <salomaodiasdiniz22@gmail.com>
2025-01-21 15:44:21 -0300

    Projeto criado

---
+++ src/main/java/com/sdd/gameslistapi/GameslistapiApplication.java
@@ -0,0 +1,13 @@
+package com.sdd.gameslistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameslistapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameslistapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sdd/gameslistapi/GameslistapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdd.gameslistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameslistapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
