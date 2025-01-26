48816c6962d0b1554cd9706a11fe48f0fad45877
Salomao Dias <salomaodiasdiniz22@gmail.com>
2025-01-26 16:29:04 -0300

    CriadO endpoint "replacement" para mudar as posições dos jogos na lista

--- src/main/java/com/sdd/gameslistapi/services/GameListService.java
+++ src/main/java/com/sdd/gameslistapi/services/GameListService.java
@@ -2,7 +2,9 @@ package com.sdd.gameslistapi.services;
 
 import com.sdd.gameslistapi.entities.GameList;
 import com.sdd.gameslistapi.gamedto.GameListDTO;
+import com.sdd.gameslistapi.projections.GameMinProjection;
 import com.sdd.gameslistapi.repositories.GameListRepository;
+import com.sdd.gameslistapi.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,9 +17,26 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll() {
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(GameListDTO::new).toList();
+    }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        int min = Math.min(sourceIndex, destinationIndex);
+        int max = Math.max(sourceIndex, destinationIndex);
+
+        for (int i = min; i <= max; i++) {
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
     }
 }
